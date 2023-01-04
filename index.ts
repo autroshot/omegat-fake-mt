7aa71aee4e77047e0b3aa0c58df85ae600730a1c
autroshot <autroshot@gmail.com>
2023-01-05 00:57:17 +0900

    첫 커밋

---
+++ index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const app = express();
+const port = 8877;
+
+app.use(express.json());
+
+app.all('/translate', function (req, res) {
+  console.log('%s %s', req.method, req.url);
+
+  let textToTranslate = req.query.text as string;
+
+  // Do something more useful here...
+  textToTranslate =
+    req.query.source +
+    '>' +
+    req.query.target +
+    ' => [' +
+    textToTranslate.toUpperCase() +
+    ']';
+
+  res.status(200).send({ translation: textToTranslate });
+});
+
+app.listen(port, function () {
+  console.log(`Fake TM Server listening on port ${port}!`);
+});
