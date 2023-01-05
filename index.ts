a0a90bb57ca9ea99835005f68fcdfeab2a2e789a
autroshot <autroshot@gmail.com>
2023-01-06 01:31:34 +0900

    구현: 유효한 요청인지 확인하는 논리

--- index.ts
+++ index.ts
@@ -8,29 +8,48 @@ const port = 8877;
 
 app.get('/', (req, res) => {
   console.log('%s %s', req.method, req.url);
-
-  const text = req.query.text as string;
-
-  axios({
-    method: 'post',
-    url: 'https://translation.googleapis.com/language/translate/v2',
-    params: {
-      q: convertTagToSingleQuotationMarks(text),
-      target: 'ko',
-      format: 'text',
-      source: 'en',
-      model: 'base',
-      key: process.env.GOOGLE_TRANSLATE_KEY,
-    },
-  })
-    .then((axiosResponse) => {
-      res.status(200).send({
-        translation: axiosResponse.data.data.translations[0].translatedText,
-      });
+  const query = req.query;
+  if (!isValidRequestQuery(query)) {
+    res.status(400).send();
+  } else {
+    axios({
+      method: 'post',
+      url: 'https://translation.googleapis.com/language/translate/v2',
+      params: {
+        q: convertTagToSingleQuotationMarks(query.text),
+        target: 'ko',
+        format: 'text',
+        source: 'en',
+        model: 'base',
+        key: process.env.GOOGLE_TRANSLATE_KEY,
+      },
     })
-    .catch((axiosError) => {
-      res.status(400);
-    });
+      .then((axiosResponse) => {
+        res.status(200).send({
+          translation: axiosResponse.data.data.translations[0].translatedText,
+        });
+      })
+      .catch((axiosError) => {
+        res.status(axiosError.response.status).send();
+      });
+  }
+
+  function isValidRequestQuery(any: any): any is ValidRequestQuery {
+    return (
+      typeof any === 'object' &&
+      !Array.isArray(any) &&
+      any !== null &&
+      any.source === 'EN-US' &&
+      any.target === 'KO' &&
+      typeof any.text === 'string'
+    );
+  }
+
+  interface ValidRequestQuery {
+    source: 'EN-US';
+    target: 'KO';
+    text: string;
+  }
 });
 
 app.listen(port, () => {
