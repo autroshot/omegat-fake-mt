62c781fc9dc5180f88c70620ff7c584029280905
autroshot <autroshot@gmail.com>
2023-01-05 23:08:22 +0900

    구현: 구글 번역 API에 액시오스로 요청하는 논리

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
+import 'dotenv/config';
 import express from 'express';
+import axios from './node_modules/axios/index';
 import { convertTagToSingleQuotationMarks } from './utils/text';
 
 const app = express();
@@ -7,19 +9,30 @@ const port = 8877;
 app.get('/', (req, res) => {
   console.log('%s %s', req.method, req.url);
 
-  let translatedText = '';
   let text = req.query.text as string;
 
-  // Do something more useful here...
-  translatedText =
-    req.query.source +
-    '>' +
-    req.query.target +
-    ' => [' +
-    convertTagToSingleQuotationMarks(text) +
-    ']';
-
-  res.status(200).send({ translation: translatedText });
+  axios({
+    method: 'post',
+    url: 'https://translation.googleapis.com/language/translate/v2',
+    params: {
+      q: convertTagToSingleQuotationMarks(text),
+      target: 'ko',
+      format: 'text',
+      source: 'en',
+      model: 'base',
+      key: process.env.GOOGLE_TRANSLATE_KEY,
+    },
+  })
+    .then((axiosResponse) => {
+      res
+        .status(200)
+        .send({
+          translation: axiosResponse.data.data.translations[0].translatedText,
+        });
+    })
+    .catch((axiosError) => {
+      res.status(400);
+    });
 });
 
 app.listen(port, () => {
