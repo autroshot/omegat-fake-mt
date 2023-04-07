fd6fccc2438e48848711ecc73140da344e7b326b
autroshot <autroshot@gmail.com>
2023-04-07 18:53:22 +0900

    테스트: 변경된 함수 이름 적용

--- utils/text.test.ts
+++ utils/text.test.ts
@@ -1,36 +1,36 @@
-import { convertTagToSingleQuotationMarks } from './text';
+import { convertTagToApostrophe } from './text';
 
-test('convertTagToSingleQuotationMarks 함수', () => {
+test('convertTagToApostrophe 함수', () => {
   expect(
-    convertTagToSingleQuotationMarks(
+    convertTagToApostrophe(
       `Type <g1>mozilla.org</g1> in your browser's location bar.`
     )
   ).toBe(`Type 'mozilla.org' in your browser's location bar.`);
   expect(
-    convertTagToSingleQuotationMarks(
+    convertTagToApostrophe(
       '<x1/>Explanation of the steps needed to obtain the result to a DNS request<x2/>'
     )
   ).toBe(
     `'Explanation of the steps needed to obtain the result to a DNS request'`
   );
   expect(
-    convertTagToSingleQuotationMarks(
+    convertTagToApostrophe(
       `Lorem Ipsum has been the industry's <g2>standard <g3>dummy</g3></g2> text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
     )
   ).toBe(
     `Lorem Ipsum has been the industry's 'standard 'dummy'' text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
   );
   expect(
-    convertTagToSingleQuotationMarks(
+    convertTagToApostrophe(
       `It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like <g11>Aldus PageMaker</g11> including versions of Lorem Ipsum.`
     )
   ).toBe(
     `It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 'Aldus PageMaker' including versions of Lorem Ipsum.`
   );
-  expect(
-    convertTagToSingleQuotationMarks(`<g1>server</g1> <-> <g2>client</g2>`)
-  ).toBe(`'server' <-> 'client'`);
-  expect(convertTagToSingleQuotationMarks(`<g1><div></g1> is tag.`)).toBe(
+  expect(convertTagToApostrophe(`<g1>server</g1> <-> <g2>client</g2>`)).toBe(
+    `'server' <-> 'client'`
+  );
+  expect(convertTagToApostrophe(`<g1><div></g1> is tag.`)).toBe(
     `'<div>' is tag.`
   );
 });
